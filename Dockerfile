2f28f0c5d99e7cb26320165adcdd2ac2c3dc1584
tdedobbeleer <tdedobbeleer@gmail.com>
2025-04-13 19:27:38 +0200

    add circleci

---
+++ Dockerfile
@@ -0,0 +1,17 @@
+FROM eclipse-temurin:19
+
+ARG JAR
+
+#Prereqs
+RUN apt update
+RUN apt install telnet -y
+
+RUN mkdir /usr/app
+RUN echo $JAR > /usr/app/release.info
+
+COPY ./$JAR /usr/app/sic.jar
+
+RUN adduser --disabled-login --disabled-password --gecos "" javauser
+USER javauser
+
+ENTRYPOINT ["java", "-Xss512K","-XX:CICompilerCount=2","-Dfile.encoding=UTF-8","-XX:+UseContainerSupport","-Djava.security.egd=file:/dev/./urandom", "-jar","/usr/app/sic.jar", "--spring.profiles.active=${SPRING_PROFILES_ACTIVE:-local}"]
---
+++ scripts/dockerbuild.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+export DOCKER_CLI_EXPERIMENTAL=enabled
+
+build () {
+  if ! docker buildx build --build-arg JAR="${JAR}" --push --platform linux/arm64/v8,linux/amd64 --tag "${DOCKER_USER}/sic:${1}" .; then
+    echo "Building tag ${1} failed miserably."
+    exit 1
+  fi
+}
+
+DATE=$( date '+%y%m%d.%H.%M.%S' )
+
+echo "Pushing docker image version ${DATE} and tagging latest"
+#Get the latest .jar
+JAR="target\sharing-is-caring-0.0.1-SNAPSHOT.jar"
+
+#Login
+echo "${DOCKER_PASSWORD}" | docker login --username $DOCKER_USER --password-stdin
+
+#Build for all archs
+docker context create buildx-build
+docker buildx create --use buildx-build
+
+build "latest"
+build "${DATE}"
